e0a4a7f05c1e696cfb40533ae7b526bb29a5c319
Andrej Korman <andrejkorman@gmail.com>
2022-07-13 00:54:42 +0200

    Add map drawing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use sdl2::event::Event;
+use sdl2::keyboard::Keycode;
+use sdl2::mouse::MouseButton;
+use sdl2::pixels::Color;
+use sdl2::rect::Point;
+use sdl2::render::Canvas;
+use sdl2::video::Window;
+
+use std::cmp::{max, min};
+use std::time::Duration;
+
 use std::collections::HashMap;
 use std::collections::HashSet;
 use std::collections::VecDeque;
@@ -83,6 +94,102 @@ fn check_connectivity(nodes: HashMap<NodeId, NodeInfo>, ways: HashMap<WayId, Way
     println!("Number of components is {}", components);
 }
 
+const WIDTH: u32 = 1600;
+const HEIGHT: u32 = 800;
+const MAX_LINE_COUNT: u32 = 500_000;
+
+struct MapDrawing {}
+
+impl MapDrawing {
+    pub fn new() -> Self {
+        Self {}
+    }
+    pub fn draw(&self, nodes: HashMap<NodeId, NodeInfo>, ways: HashMap<WayId, WayInfo>) {
+        let sdl_context = sdl2::init().unwrap();
+        let video_subsystem = sdl_context.video().unwrap();
+        let window = video_subsystem
+            .window("rust-sdl2 demo", WIDTH, HEIGHT)
+            .position_centered()
+            .build()
+            .unwrap();
+        let mut canvas = window.into_canvas().build().unwrap();
+        let mut event_pump = sdl_context.event_pump().unwrap();
+
+        'running: loop {
+            canvas.set_draw_color(Color::RGB(255, 255, 255));
+            canvas.clear();
+            canvas.set_draw_color(Color::RGB(255, 0, 0));
+            // drawing
+            let mut draw_counter = 0;
+            let mut to_draw: Vec<(&NodeInfo, &NodeInfo)> = Vec::new();
+            for (_, way_info) in ways.iter() {
+                draw_counter += 1;
+                if draw_counter == MAX_LINE_COUNT {
+                    break;
+                }
+                for i in 0..way_info.nodes.len() - 1 {
+                    let from_id = way_info.nodes[i];
+                    let to_id = way_info.nodes[i + 1];
+
+                    let node_info_from = nodes.get(&from_id).unwrap();
+                    let node_info_to = nodes.get(&to_id).unwrap();
+
+                    to_draw.push((node_info_from, node_info_to));
+                }
+            }
+
+            let mut min_lat = 1_000_000_000;
+            let mut max_lat = 0;
+            let mut min_lon = 1_000_000_000;
+            let mut max_lon = 0;
+            for (from, to) in to_draw.iter() {
+                min_lat = min(min_lat, from.decimicro_lat);
+                min_lon = min(min_lon, from.decimicro_lon);
+                max_lat = max(max_lat, from.decimicro_lat);
+                max_lon = max(max_lon, from.decimicro_lon);
+
+                min_lat = min(min_lat, to.decimicro_lat);
+                min_lon = min(min_lon, to.decimicro_lon);
+                max_lat = max(max_lat, to.decimicro_lat);
+                max_lon = max(max_lon, to.decimicro_lon);
+            }
+
+            let lat_diff = (max_lat - min_lat) as f64;
+            let lon_diff = (max_lon - min_lon) as f64;
+
+            for (from_node, to_node) in to_draw.iter() {
+                let mut a = ((from_node.decimicro_lat - min_lat) as f64) / lat_diff;
+                let mut b = ((from_node.decimicro_lon - min_lon) as f64) / lon_diff;
+                let mut c = ((to_node.decimicro_lat - min_lat) as f64) / lat_diff;
+                let mut d = ((to_node.decimicro_lon - min_lon) as f64) / lon_diff;
+
+                a *= HEIGHT as f64;
+                b *= WIDTH as f64;
+                c *= HEIGHT as f64;
+                d *= WIDTH as f64;
+
+                let from = Point::new(b as i32, HEIGHT as i32 - (a as i32));
+                let to = Point::new(d as i32, HEIGHT as i32 - (c as i32));
+                canvas.draw_line(from, to).unwrap();
+            }
+
+            for event in event_pump.poll_iter() {
+                match event {
+                    Event::Quit { .. }
+                    | Event::KeyDown {
+                        keycode: Some(Keycode::Escape),
+                        ..
+                    } => break 'running,
+                    _ => {}
+                }
+            }
+
+            canvas.present();
+            ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
+        }
+    }
+}
+
 fn main() {
     let f = File::open("data/slovakia-latest.osm.pbf").unwrap();
     let mut pbf = osmpbfreader::OsmPbfReader::new(f);
@@ -137,5 +244,8 @@ fn main() {
     nodes.shrink_to_fit();
     ways.shrink_to_fit();
 
-    check_connectivity(nodes, ways);
+    //check_connectivity(nodes, ways);
+
+    let draw = MapDrawing::new();
+    draw.draw(nodes, ways);
 }
